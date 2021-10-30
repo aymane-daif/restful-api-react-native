ef30ef2e377aa86e1fa248d5a8a0f3798cb6cd20
Aymane Daif <aymaned345@gmail.com>
2021-10-30 14:22:03 +0100

    :v: setup client code

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'steelblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
