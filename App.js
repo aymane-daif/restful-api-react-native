7e9efde13dd96917d9954332d9b4e6a213571e52
Aymane Daif <aymaned345@gmail.com>
2021-10-31 13:28:19 +0100

    :satisfied: add sign up page to UI

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Dimensions,
+  ImageBackground,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import FormData from './components/FormData';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style='auto' />
-    </View>
+    <SafeAreaView>
+      <ImageBackground
+        blurRadius={2}
+        resizeMode='cover'
+        source={require('./assets/welcome.jpg')}
+        style={styles.background}
+      >
+        <View style={styles.overlay}>
+          <Navbar />
+          <Text style={styles.heading}>Inscription</Text>
+          <FormData />
+        </View>
+        <StatusBar style='auto' />
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
+    backgroundColor: '#56BC58',
+    height: Dimensions.get('window').height,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight - 15 : 0,
+  },
+  heading: {
+    color: '#f5f5f5',
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Apple',
+    fontSize: 26,
+    fontWeight: 'bold',
+    letterSpacing: 1,
+    margin: 20,
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0,.6)',
     flex: 1,
-    backgroundColor: 'steelblue',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
