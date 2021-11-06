65910bf43569b488ad16a37d6b85f77af6a41c85
Aymane Daif <aymaned345@gmail.com>
2021-11-06 14:56:06 +0100

    add repo interface and implementation

---
+++ server/BankAccount/src/main/java/ma/ensa/api/BankAccount/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package ma.ensa.api.BankAccount.repository;
+
+import ma.ensa.api.BankAccount.model.User;
+
+public interface UserRepository {
+    int save(User user);
+}
---
+++ server/BankAccount/src/main/java/ma/ensa/api/BankAccount/repository/UserRepositoryImpl.java
@@ -0,0 +1,28 @@
+package ma.ensa.api.BankAccount.repository;
+
+import ma.ensa.api.BankAccount.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class UserRepositoryImpl implements UserRepository{
+
+    private static final String INSERT_USER_QUERY ="INSERT INTO USER(id, firstName, lastName, email, address, phone, cin) VALUES(?,?,?,?,?,?,?)";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public int save(User user) {
+        jdbcTemplate.update(
+                INSERT_USER_QUERY,
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getAddress(),
+                user.getPhone(),
+                user.getCin()
+        );
+        return user.getId();
+    }
+}
