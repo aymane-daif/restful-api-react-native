20a749b11a55c6a4c11ea9174d0c0bd39e1c7245
Aymane Daif <aymaned345@gmail.com>
2021-11-06 14:42:08 +0100

    add User Entity

---
+++ server/BankAccount/src/main/java/ma/ensa/api/BankAccount/model/User.java
@@ -0,0 +1,94 @@
+package ma.ensa.api.BankAccount.model;
+
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String phone;
+    private String cin;
+
+    public User() {
+    }
+
+    public User(int id, String firstName, String lastName, String email, String address, String phone, String cin) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+        this.cin = cin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", cin='" + cin + '\'' +
+                '}';
+    }
+}
