63f7a3ed70d7adf78cef5b14e88d660d865a18cb
Aymane Daif <aymaned345@gmail.com>
2021-11-06 15:06:58 +0100

    add user service

---
+++ server/BankAccount/src/main/java/ma/ensa/api/BankAccount/service/UserService.java
@@ -0,0 +1,17 @@
+package ma.ensa.api.BankAccount.service;
+
+import ma.ensa.api.BankAccount.model.User;
+import ma.ensa.api.BankAccount.repository.UserRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepositoryImpl userRepositoryImpl;
+
+    public int save(User user){
+        return userRepositoryImpl.save(user);
+    }
+}
